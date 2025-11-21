ad9c51f0e425e76b47146fba95ac1150546789f4
Duncan Smith <smithduncan40@gmail.com>
2025-11-20 23:09:20 -0500

    File structure setup/Restored .env.example

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import bookRoutes from './routes/bookRoutes.js';
+import authorRoutes from './routes/authorRoutes.js';
+import genreRoutes from './routes/genreRoutes.js';
+import reservationRoutes from './routes/reservationRoutes.js';
+import loanRoutes from './routes/loanRoutes.js';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
@@ -9,6 +17,14 @@ app.use(cors());
 app.use(morgan('tiny'));
 
 app.use(express.json());
+
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/books', bookRoutes);
+app.use('/authors', authorRoutes);
+app.use('/genres', genreRoutes);
+app.use('/reservations', reservationRoutes);
+app.use('/loans', loanRoutes);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
